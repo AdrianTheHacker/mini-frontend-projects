c6f261bb5535629bb7dbc0beec7222ef63c2e651
AdrianTheHacker <adriantarantino2006@gmail.com>
2022-10-02 13:55:57 -0400

    Completing the number guessing game

--- num_guess_game/main.js
+++ num_guess_game/main.js
@@ -1,23 +1,24 @@
-const number = 10
+const number = Math.floor(Math.random() * 10) + 1;
 
 
 const number_box = document.getElementById("number-box")
 const submit_box = document.getElementById("submit-box")
                            .addEventListener("click", () => {check_guess()})
 
+const results_box = document.getElementById("results-box")
+
 
 function check_guess() {
     const guess = number_box.value;
+    let message = "You Win!"
 
     if(guess > number) {
-        console.log("Guess Lower!")
+        message = "Guess Lower"
     } 
     
     else if(guess < number) {
-        console.log("Guess Higher!")
+        message = "Guess Higher"
     }
 
-    else {
-        console.log("You Win!")
-    }
+    results_box.innerHTML = message
 }
